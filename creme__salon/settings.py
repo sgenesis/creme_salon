161e996ed3f8272b0fb66a956026335ad94d1656
sgenesis <snemesis63@gmail.com>
2026-01-04 21:10:28 -0600

    chore: limpiar archivos no versionables para producción

--- creme__salon/settings.py
+++ creme__salon/settings.py
@@ -211,9 +211,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
 ]
-CSRF_TRUSTED_ORIGINS = [
-    "https://cremesalon-production.up.railway.app",
-]
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS'),
 
 # -------------------------------------------------------------
 # CONFIG REST FRAMEWORK
